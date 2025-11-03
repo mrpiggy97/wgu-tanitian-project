2985f4b6f53b586268654ab9137e192e2e304eef
mrpiggy97 <fabyjesusrivas10@gmail.com>
2025-11-03 00:19:46 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <nav>
+        <a href="">Home</a>
+        <a href="">Activities</a>
+        <a href="">Booking</a>
+        <a href="">Transportation</a>
+        <a href="">Important Information</a>
+      </nav>
+      <div id='views'>
+
+      </div>
+      <footer>
+        <small>Tanitian Island 2025</small>
+      </footer>
+    </div>
+  )
+}
+
+export default App
