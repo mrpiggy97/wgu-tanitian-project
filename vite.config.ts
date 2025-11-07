c99c921d61301421993a9bb15fba82940439d240
mrpiggy97 <fabyjesusrivas10@gmail.com>
2025-11-07 17:12:31 -0400

    adds configurations for the proper deployment to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/wgu-tanitian-project/', // Add base URL for GitHub Pages
   plugins: [
     react({
       babel: {
